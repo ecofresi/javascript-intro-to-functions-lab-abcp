027b9ec7bda916d6a912de464a68b9a03cb63845
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-22 23:26:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,14 +33,14 @@ function sayHiToGrandma(string) {
   var lowercase = string.toLowerCase
   var uppercase = string.toUpperCase
   
-  if (string === lowercase) {
-      return "I can\'t hear you!"
+  if (string == lowercase) {
+      return "I can\'t hear you!";
   } 
-  else if (string === uppercase) {
-      return "YES INDEED!"
+  else if (string == uppercase) {
+      return "YES INDEED!";
   } 
-  else if (string === "I love you, Grandma.") {
-      return "I love you, too."
+  else if (string == "I love you, Grandma.") {
+      return "I love you, too.";
   }
   
 }
